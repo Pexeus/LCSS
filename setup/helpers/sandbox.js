eca7d00eb8d26d71a64aa317edac4c93ebef6263
unknown <libe>
2020-07-08 18:39:24 +0200

    unifySetupFix

--- setup/helpers/sandbox.js
+++ setup/helpers/sandbox.js
@@ -1,16 +1,14 @@
 const path = require("path");
 const { displayProjects } = require("./projects");
 
-const ENCODED_DIR = __dirname.replace(/ /g, "%20");
+const ENCODED_DIR = remote.app.getAppPath().replace(/ /g, "%20");
 
 function displaySandbox() {
     let sandboxes = [];
 
     sandboxes[0] = {
-        path: path.join(__dirname, "..", "/data/sandbox/"),
-        url:
-            "file:///" +
-            path.join(ENCODED_DIR, "..", "/data/sandbox/index.html"),
+        path: path.join(remote.app.getAppPath(), "/data/sandbox/"),
+        url: "file:///" + path.join(ENCODED_DIR, "/data/sandbox/index.html"),
     };
 
     console.log(sandboxes[0]);
